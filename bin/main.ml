c7bdf829f6c9052be061fc63d9aa76cbdfcf19c2
Tony Chen <tc448@cornell.edu>
2022-10-15 14:42:53 -0400

    Updated dune files

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,7 @@
-(* This file was referenced from CS 3110 A2 assignment. *)
+(* This file was referenced using FA22 CS 3110 A2 assignment. *)
 
-(** [play_game] starts the wordbite game. *)
-let play_game =
-  ANSITerminal.print_string [ ANSITerminal.red ] "The game has started."
+(** [play_game s] starts the Wordbite game. *)
+let play_game s = raise (Failure "Unimplemented: Main.play_game")
 (* 1. Generate lists of string of letters *)
 (* 2. Call a recursive function to prompt user to enter in a string *)
 (* 3. Parse the string -> if string consists of strings from list, then check if
@@ -13,16 +12,12 @@ let play_game =
 
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
-  ANSITerminal.print_string [ ANSITerminal.red ] "\n\nWelcome to Wordbite.\n";
-  print_endline "Please type start\n";
+  ANSITerminal.print_string [ ANSITerminal.cyan ] "\n\nWelcome to Wordbite!.\n";
+  print_endline "Please type start.\n";
   print_string "> ";
   match read_line () with
   | exception End_of_file -> ()
-  | string ->
-      if string = "start" then play_game
-      else
-        ANSITerminal.print_string [ ANSITerminal.red ]
-          "\nPlease type start correctly...\n"
+  | string -> play_game string
 
 (* Execute the game engine. *)
 let () = main ()
