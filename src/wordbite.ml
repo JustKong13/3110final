06b10e8677afb501cc9809e76fa636b44e35060b
Jasmine <jqd3@cornell.edu>
2022-10-17 03:28:26 -0400

    Added banned combinations

--- src/wordbite.ml
+++ src/wordbite.ml
@@ -6,10 +6,24 @@ type game = {
 
 let alphabet_double_vowels = "aabcdeefghiijklmnoopqrstuuvwxyz"
 
+let file_to_list filename =
+  let ic = open_in filename and l = ref [] in
+  let rec loop () =
+    let line = input_line ic in
+    l := line :: !l;
+    loop ()
+  in
+  try loop ()
+  with End_of_file ->
+    close_in ic;
+    List.rev !l
+
+let banned = file_to_list "banned.txt"
+
 let get_letter a_string =
   String.make 1 (String.get a_string (Random.int (String.length a_string)))
 
-let available_strs_aux a_string =
+let rec available_strs_aux a_string =
   let size = Random.int 2 in
   if size = 0 then get_letter a_string
   else
@@ -18,7 +32,9 @@ let available_strs_aux a_string =
       Str.global_replace (Str.regexp first_letter) "" a_string
     in
     let second_letter = get_letter new_alphabet_string in
-    first_letter ^ second_letter
+    if List.mem (first_letter ^ second_letter) banned then
+      available_strs_aux a_string
+    else first_letter ^ second_letter
 
 let rec available_strs a_string accumulated_lst =
   if List.length accumulated_lst < 10 then
