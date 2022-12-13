b2613cfaaa9932b5fd426e9d138da7b3875af04f
Justin Kong <jk2338@cornell.edu>
2022-12-13 18:09:34 -0500

    added test cases for borad generation

--- test/main.ml
+++ test/main.ml
@@ -2,20 +2,20 @@
     [dune.exec ./test/main.exe (mac) dune exec test/main.exe (other)] IN THE
     TERMINAL*)
 
-(** Since our project was a game, that means that a lot of our actual testing
-    came through user testing, that is playing the game itself and finding any
-    potential bugs. However, that is difficult to test for in a test suite, so a
-    lot of our testing in this file was localized testing. We tested backend
-    functionality for or game by testing the helpers that we wrote, and we did
-    this by creating arbitrary game states which we can predict the behavior of.
-    Our game states are generated randomly, which is why we needed to create our
-    own game state. Also, since our game state was mutable, it was difficult to
-    test whether the functions that mutate game state were actually doing the
-    job without ruining other test cases. The way we got around this was by
-    writing complementary functions that returned the new game state rather than
-    mutate the game state. These functions were meant primarally to test
-    functionality, and were ultimately omitted from the actual game in favor of
-    functions that mutated game state. *)
+(** TESINNG PLAN: Since our project was a game, that means that a lot of our
+    actual testing came through user testing. However, that is difficult to test
+    for in a test suite, so a lot of our testing in this file was localized
+    testing (i.e. testing backend functions). We tested backend functionality
+    for or game by testing the helpers that we wrote, and we did this by
+    creating arbitrary game states which we can predict the behavior of. Our
+    game states are generated randomly, which is why we needed to create our own
+    game state. Also, since our game state was mutable, it was difficult to test
+    whether the functions that mutate game state were actually doing the job
+    without ruining other test cases. The way we got around this was by writing
+    complementary functions that returned the new game state rather than mutate
+    the game state. These functions were meant primarally to test functionality,
+    and were ultimately omitted from the actual game in favor of functions that
+    mutated game state. *)
 
 open OUnit2
 open Game
@@ -52,7 +52,8 @@ module B = Board
 let b1 = B.empty
 
 (*Word Validator and Board Tests*)
-
+(* These tests are to ensure our word validator was correct, as well as test
+   basic functionality with creating boards*)
 let word_validator_tests =
   [
     test "banana is a real word" (check_word "banana") true;
@@ -155,8 +156,35 @@ open Tile
 let list_of_tiles =
   [
     { tstring = "a"; ttype = ATile; position = (0, 0) };
-    { tstring = "sa"; ttype = HTile; position = (7, 7) };
+    { tstring = "sa"; ttype = HTile; position = (6, 7) };
     { tstring = "sa"; ttype = VTile; position = (5, 5) };
+  ]
+
+let tile_list_1 =
+  [
+    { tstring = "g"; ttype = ATile; position = (3, 8) };
+    { tstring = "o"; ttype = ATile; position = (6, 4) };
+    { tstring = "uo"; ttype = VTile; position = (2, 4) };
+    { tstring = "it"; ttype = HTile; position = (3, 3) };
+    { tstring = "ap"; ttype = VTile; position = (5, 5) };
+    { tstring = "i"; ttype = ATile; position = (2, 7) };
+    { tstring = "c"; ttype = ATile; position = (0, 8) };
+    { tstring = "y"; ttype = ATile; position = (7, 3) };
+    { tstring = "mi"; ttype = HTile; position = (0, 2) };
+    { tstring = "ao"; ttype = HTile; position = (6, 8) };
+  ]
+
+let list_of_tiles1_board =
+  [
+    [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
+    [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
+    [ "m"; "i"; "-"; "-"; "-"; "-"; "-"; "-" ];
+    [ "-"; "-"; "-"; "i"; "t"; "-"; "-"; "y" ];
+    [ "-"; "-"; "u"; "-"; "-"; "-"; "o"; "-" ];
+    [ "-"; "-"; "o"; "-"; "-"; "a"; "-"; "-" ];
+    [ "-"; "-"; "-"; "-"; "-"; "p"; "-"; "-" ];
+    [ "-"; "-"; "i"; "-"; "-"; "-"; "-"; "-" ];
+    [ "c"; "-"; "-"; "g"; "-"; "-"; "a"; "o" ];
   ]
 
 let tile_tests =
@@ -165,11 +193,11 @@ let tile_tests =
       (find_tile (0, 0) list_of_tiles)
       ({ tstring = "a"; ttype = ATile; position = (0, 0) }, true);
     test "finding tile loc 2"
-      (find_tile (7, 7) list_of_tiles)
-      ({ tstring = "sa"; ttype = HTile; position = (7, 7) }, true);
+      (find_tile (6, 7) list_of_tiles)
+      ({ tstring = "sa"; ttype = HTile; position = (6, 7) }, true);
     test "finding adjacent tile of HTile"
-      (find_tile (8, 7) list_of_tiles)
-      ({ tstring = "sa"; ttype = HTile; position = (7, 7) }, false);
+      (find_tile (7, 7) list_of_tiles)
+      ({ tstring = "sa"; ttype = HTile; position = (6, 7) }, false);
     test "finding adjacent tile of VTile"
       (find_tile (5, 6) list_of_tiles)
       ({ tstring = "sa"; ttype = VTile; position = (5, 5) }, false);
@@ -179,41 +207,48 @@ let tile_tests =
       (move (0, 0) (2, 2) list_of_tiles)
       [
         { tstring = "a"; ttype = ATile; position = (2, 2) };
-        { tstring = "sa"; ttype = HTile; position = (7, 7) };
+        { tstring = "sa"; ttype = HTile; position = (6, 7) };
         { tstring = "sa"; ttype = VTile; position = (5, 5) };
       ];
     test_exception "moving to overlap" Game.Wordbite.TileOverlap (fun _ ->
-        move (0, 0) (7, 7) list_of_tiles);
+        move (0, 0) (6, 7) list_of_tiles);
     test_exception "moving to overlap adjacent" Game.Wordbite.TileOverlap
-      (fun _ -> move (0, 0) (8, 7) list_of_tiles);
+      (fun _ -> move (0, 0) (7, 7) list_of_tiles);
     test "moving next to a tile valid"
       (move (0, 0) (7, 8) list_of_tiles)
       [
         { tstring = "a"; ttype = ATile; position = (7, 8) };
-        { tstring = "sa"; ttype = HTile; position = (7, 7) };
+        { tstring = "sa"; ttype = HTile; position = (6, 7) };
         { tstring = "sa"; ttype = VTile; position = (5, 5) };
       ];
     test_exception "moving and then checking location of the original position"
       Game.Wordbite.TileNotFound (fun _ ->
         find_tile (0, 0) (move (0, 0) (2, 2) list_of_tiles));
+    test "generate empty board"
+      (B.board_to_list (generate_game_board [] B.empty))
+      (B.board_to_list B.empty);
+    test "generate correct board given tile list"
+      (B.board_to_list (generate_game_board list_of_tiles B.empty))
+      [
+        [ "a"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
+        [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
+        [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
+        [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
+        [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
+        [ "-"; "-"; "-"; "-"; "-"; "s"; "-"; "-" ];
+        [ "-"; "-"; "-"; "-"; "-"; "a"; "-"; "-" ];
+        [ "-"; "-"; "-"; "-"; "-"; "-"; "s"; "a" ];
+        [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
+      ];
+    test "generate correct board given tile list randomized"
+      (B.board_to_list (generate_game_board tile_list_1 B.empty))
+      list_of_tiles1_board;
   ]
 
 let find_words_of_row (lst : string list) =
   lst |> create_string_of_row |> generate_list_of_words |> get_valid_words
 
-let board1 =
-  [
-    [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
-    [ "-"; "b"; "-"; "-"; "-"; "-"; "-"; "-" ];
-    [ "-"; "a"; "-"; "-"; "-"; "-"; "-"; "-" ];
-    [ "-"; "n"; "o"; "t"; "-"; "-"; "-"; "-" ];
-    [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
-    [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
-    [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
-    [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
-    [ "-"; "-"; "-"; "-"; "-"; "-"; "-"; "-" ];
-  ]
-
+(* creates a board with the word ban horizontally and not vertically*)
 let board1_rep =
   B.empty
   |> B.place_letter "b" (1, 1)
